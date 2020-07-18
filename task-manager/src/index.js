a51fc1abe06ee74b2f7aebe50fc001d47186d0a7
leandrofahur <leandrofahur@gmail.com>
2020-07-17 19:11:37 -0700

    first Rest API operation

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,21 @@
-const express = require('express')
+require('./db/mongoose');
+const express = require('express');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+
+app.post('/users', (req, res) => {
+  const task = new Task(req.body);
+  task.save().then(() => {
+    res.send(task);
+  }). catch( err => {
+    console.log(err);
+  })
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
