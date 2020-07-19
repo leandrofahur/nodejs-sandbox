f27c61b0b89608634f1cdc32cc5abe7b564a13c0
leandrofahur <leandrofahur@gmail.com>
2020-07-19 09:50:06 -0700

    funny experiment

---
+++ dgram-playground/dgram-client.js
@@ -0,0 +1,43 @@
+const dgram = require('dgram');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const client = {
+  address: '192.168.10.1',
+  port: 8889
+};
+
+const socket = dgram.createSocket('udp4');
+
+socket.on('error', (err) => {
+  if(err) {
+    console.log(err);
+    socket.onClose();
+  }
+});
+  
+socket.on('message', (message) => {
+  if(message) {
+    console.log(`\nMessage: ${message}`);
+  }
+});  
+
+(function userInput() {
+  readline.question('Enter command:  ', (command) => {
+    if (command === 'close') {
+      console.log('Closing aplication...');
+      socket.close();
+      readline.close();
+    } else {
+      socket.send(command, client.port, client.address, (err, bytes) => {
+        if(err) {
+          console.log(err.message);
+          socket.close();
+        }
+      });
+      userInput();
+    }
+  });
+}());
---
+++ dgram-playground/dgram-server.js
@@ -0,0 +1,23 @@
+const dgram = require('dgram');
+
+const server = {
+  address: '0.0.0.0',
+  port: 11111
+};
+
+const socket = dgram.createSocket('udp4');
+
+socket.on('error', (err) => {
+  if(err) {
+    console.log(err);
+    socket.onClose();
+  }
+});
+  
+socket.on('message', (message) => {
+  if(message) {
+    console.log(typeof emessage);
+  }
+});  
+
+socket.bind(server.port, server.address);
