a89af1224f331495d5572161e8ec9b35fca766e2
leandrofahur <leandrofahur@gmail.com>
2020-07-18 19:12:36 -0700

    powerful techniques to use in javascript

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndUpdate('5f125f62f059904d25ddffb3', { completed: true }).then((res) => {
-  console.log(res);
-  return Task.countDocuments({ completed: true });
-}).then((res,rej) => {
-  console.log(res);
-}).catch(err => {
-  console.log(err);
+// Task.findByIdAndUpdate('5f125f62f059904d25ddffb3', { completed: true }).then((res) => {
+//   console.log(res);
+//   return Task.countDocuments({ completed: true });
+// }).then((res,rej) => {
+//   console.log(res);
+// }).catch(err => {
+//   console.log(err);
+// })
+
+const updateTaskIsCompletedToFalse = async (id, completed) => {
+  const taskCompleted = await Task.findByIdAndUpdate('5f125f72f059904d25ddffb4', { completed: false });
+  const count = await Task.countDocuments({ complete: true });
+  return {
+    completed: taskCompleted,
+    count: count
+  };
+}
+
+updateTaskIsCompletedToFalse().then(res => {
+  console.log(res.completed);
+  console.log(res.count);
 })
