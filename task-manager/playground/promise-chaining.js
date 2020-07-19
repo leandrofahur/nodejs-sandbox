30e692afc92e9d58a143712290cce542b48d9893
leandrofahur <leandrofahur@gmail.com>
2020-07-18 19:17:52 -0700

    async and await to delete item from mongodb and then count the amount of documents with the completed task true

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -12,10 +12,13 @@ const Task = require('../src/models/task');
 
 const deleteTaskAndCount = async (id) => {
   const task = await Task.findByIdAndDelete(id);
-  const count = await Task.countDocuments({ complete: true });
+  if(!task) throw new Error(`There is no such task with the ${id}`);
+  const count = await Task.countDocuments({ completed: false });
   return count;
 }
 
 deleteTaskAndCount('5f125f72f059904d25ddffb4').then(count => {
   console.log(count);
+}).catch(err => {
+  console.log(err);
 })
