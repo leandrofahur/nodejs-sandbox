7f28807af8b36da082ea61b270959ab369222710
leandrofahur <leandrofahur@gmail.com>
2020-07-18 18:58:57 -0700

    the power of sync await with the error handler

--- sandbox/async-await.js
+++ sandbox/async-await.js
@@ -1,7 +1,11 @@
 const add = (a, b) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(a+b);
+      if(a < 0 || b < 0) {
+        return reject('Numbers must be non-negative!')
+      } else {
+        resolve(a+b);
+      }
     }, 1000);
   }); 
 };
