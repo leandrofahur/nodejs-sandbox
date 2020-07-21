f6f8404338df6fb180483769cb00e4c215734fc4
leandrofahur <leandrofahur@gmail.com>
2020-07-21 14:43:18 -0700

    routing data

--- sandbox/requests-basics.js
+++ sandbox/requests-basics.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit();
+  const url = req.url;
+  if(url === '/') {
+    res.setHeader('content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write(`
+      <body>
+        <h1>Insert an message</h1>
+        <form action="/message" method="POST">
+          <input type="text" name="message">
+          <button type="submit">Submit</button>
+        </form>
+      </body>`);
+    res.write('</html>');
+    return res.end();  
+  }
   res.setHeader('content-type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
