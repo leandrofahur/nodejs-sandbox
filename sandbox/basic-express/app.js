df51cf463b3cc1d8eec98f13e009d0a4a71de7b5
leandrofahur <leandrofahur@gmail.com>
2020-07-21 22:59:08 -0700

    a peek on middleware

--- sandbox/basic-express/app.js
+++ sandbox/basic-express/app.js
@@ -1,1 +1,19 @@
-console.log('Sanity check...');
+// Core modules:
+const http = require('http');
+
+// Third-party modules:
+const express = require('express');
+
+
+const app = express();
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 2!');
+});
+
+const server = http.createServer(app);
+server.listen(3000);
