e2504b631fdeea15c11b8e1e0841993361e48b72
leandrofahur <leandrofahur@gmail.com>
2020-07-21 23:26:51 -0700

    body parser =O

--- sandbox/basic-express/app.js
+++ sandbox/basic-express/app.js
@@ -3,17 +3,36 @@ const http = require('http');
 
 // Third-party modules:
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
+app.use(bodyParser.urlencoded());
+
+app.use('/', (req, res, next) => {
+  // console.log('This always runs!');
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 2!');
+app.use('/add-product', (req, res, next) => {
+  res.send(`<body>
+  <h1>Insert an message</h1>
+  <form action="/product" method="POST">
+  <input type="text" name="product">
+  <button type="submit">Submit</button>
+  </form>
+  </body>`);
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/product', (req, res, next) => {
+  console.log(req.body.product);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express...</h1>');
+  next();
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening at port 3000');
+})
