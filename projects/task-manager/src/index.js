658921e44b9e8c20fada930b1f2bfa36646c63a9
leandrofahur <leandrofahur@gmail.com>
2020-07-23 17:57:38 -0700

    added user model and router

--- projects/task-manager/src/index.js
+++ projects/task-manager/src/index.js
@@ -1,12 +1,18 @@
 require('./db/mongoose');
 const express = require('express');
 const taskRouter = require('./routers/task');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter);
 app.use(taskRouter);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to nodejs task app</h1>');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
