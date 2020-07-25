c53593f57882fdad4296a6624096c5104574b1b4
leandrofahur <leandrofahur@gmail.com>
2020-07-25 01:41:08 -0700

    submit search using the form

--- sandbox/web-server/public/js/app.js
+++ sandbox/web-server/public/js/app.js
@@ -1,10 +1,16 @@
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-  response.json().then(data => {
-    if(data.error) {
-      return console.log(data.error);
-    }
-    
-    console.log(data.location);
-    console.log(data.forecast);
+const weatherForm = document.querySelector('form');
+const address = document.getElementById('address');
+
+weatherForm.addEventListener('submit', (evt) => {
+  // evt.preventDefault();
+  fetch(`http://localhost:3000/weather?address=${address.value}`).then((response) => {
+    response.json().then(data => {
+      if(data.error) {
+        return console.log(data.error);
+      }
+      
+      console.log(data.location);
+      console.log(data.forecast);
+    });
   });
-});
+})
