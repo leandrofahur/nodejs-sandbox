64c7b3984f03b567a1815ccebb1b498024012d3f
leandrofahur <leandrofahur@gmail.com>
2020-07-24 20:54:37 -0700

    templating with handlebars

--- sandbox/web-server/src/app.js
+++ sandbox/web-server/src/app.js
@@ -5,20 +5,40 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Weather App"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About Me",
+    name: "Leandro machado"
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: "Help",
+    message: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, facilis exercitationem. Iusto, culpa? Inventore, consequatur."
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: "It is sunny for now",
     location: 'Surrey'
   })
-})
+});
 
 app.get('*', (req, res) => {
   res.send('Error Page')
-})
-
+});
 
 app.listen(3000, () => {
   console.log('Server is up and listening port 3000');
-})
+});
