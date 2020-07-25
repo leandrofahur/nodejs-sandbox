1a4dc26d5c4d2b51680ea713a14ba0d3910d5e31
leandrofahur <leandrofahur@gmail.com>
2020-07-24 19:39:31 -0700

    styling the .html

--- sandbox/web-server/src/app.js
+++ sandbox/web-server/src/app.js
@@ -7,22 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-})
-
-app.get('/help', (req, res) => {
-  res.send([{
-    name: "Leandro"
-  }, {
-    name: "Sarah"
-  }])
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About Page<h1>')
-})
-
 app.get('/weather', (req, res) => {
   res.send({
     forecast: "It is sunny for now",
