f1ed7c1fdefc10045ba60abedcb12eafd7e8d20e
leandrofahur <leandrofahur@gmail.com>
2020-07-25 01:20:08 -0700

    first fetch from my JSON api

--- sandbox/web-server/public/js/app.js
+++ sandbox/web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log('Client side javascript file is loaded...');
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+  response.json().then(data => {
+    if(data.error) {
+      return console.log(data.error);
+    }
+    
+    console.log(data.location);
+    console.log(data.forecast);
+  });
+});
